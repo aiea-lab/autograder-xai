36a99ae695e41f8ad13f833fa637fd003afc77af
aoue <aajaouellette@gmail.com>
2024-03-01 11:23:23 -0800

    Trying to fix data retrieval from data.py, working locally

--- data.py
+++ data.py
@@ -1,5 +1,9 @@
-class Data:
-    def __init__(self) -> None:
-        data = {
-            "p0_q1_0": "make sure to calculate cost based on weight AND price of each fruit."
-        }
+# class Data:
+#     def __init__(self) -> None:
+#         self.data = {
+#             "p0_q1_0": "make sure to calculate cost based on weight AND price of each fruit."
+#         }
+
+data = {
+    "p0_q1_0": "make sure to calculate cost based on weight AND price of each fruit."
+}
--- explainer.py
+++ explainer.py
@@ -1,4 +1,4 @@
-from data import Data
+from data import data
 
 class Explainer:
     def __init__(self, course, assignment, question):
@@ -6,13 +6,18 @@ class Explainer:
         self.assignment = assignment
         self.question = question
         self.messages = []
-        self.data = Data()
+        self.data = data
 
     # need to also implement the some_info input
-    def get_feedback(self, default="TODO"):
-        return default + "\n> " + self.get_explanation()
+    def get_feedback(self, default="TODO", issue_id=-1):
+        return default + "\n> " + self.get_explanation(issue_id)
     
     def get_explanation(self, issue_id):
-        if issue_id in data:
-            return self.data[issue_id]
-        return "sample explanation"
+        key = f"{self.assignment}_{self.question}_{issue_id}"
+        if key in self.data:
+            return self.data[key]
+        return f"Error: No explanation found for {key}."
+
+# e = Explainer("cse140", "p0", "q1")
+# f = e.get_feedback("ERROR: you failed the example!", 0)
+# print(f)
