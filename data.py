259fc0507da94c887878e7db2a4e062923dce515
aoue <aajaouellette@gmail.com>
2024-03-01 11:43:59 -0800

    data import working properly and fully covers for p0

--- data.py
+++ data.py
@@ -1,9 +1,5 @@
-# class Data:
-#     def __init__(self) -> None:
-#         self.data = {
-#             "p0_q1_0": "make sure to calculate cost based on weight AND price of each fruit."
-#         }
-
 data = {
-    "p0_q1_0": "make sure to calculate cost based on weight AND price of each fruit."
+    "p0_q1_0": "make sure to calculate cost based on weight AND price of each fruit.",
+    "p0_q2_0": "make sure to consider the total cost of the order for both apples AND oranges at each shop.",
+    "p0_q2_1": "make sure to consider the total cost of the order for both apples AND oranges at each shop."
 }
