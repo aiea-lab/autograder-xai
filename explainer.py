32e1a8f587240357503ae122d4f5b59e841602f4
dhsieh4 <114768882+dhsieh4@users.noreply.github.com>
2024-03-06 15:20:35 -0800

    Add CSV reading and CSV to data dictionary function

--- explainer.py
+++ explainer.py
@@ -1,12 +1,16 @@
-from autograderXAI.data import data
+import csv
 
 class Explainer:
-    def __init__(self, course, assignment, question):
+    def __init__(self, course, assignment, question, csv_file):
         self.course = course
         self.assignment = assignment
         self.question = question
         self.messages = []
-        self.data = data
+        self.data = []
+        with open(csv_file, mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                self.data.append(row)
 
     # need to also implement the some_info input
     def get_feedback(self, default="TODO", issue_id=-1):
@@ -17,6 +21,9 @@ class Explainer:
         if key in self.data:
             return self.data[key]
         return f"Error: No explanation found for {key}."
+    
+    def get_data(self):
+        return self.data
 
 # e = Explainer("cse140", "p0", "q1")
 # f = e.get_feedback("ERROR: you failed the example!", 0)
