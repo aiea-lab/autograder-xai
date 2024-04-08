e2ef43ff7e67fbe3fcefc89e21d60e33f06a7f1d
aoue <aajaouellette@gmail.com>
2024-04-08 13:05:07 -0700

    Remove unused data.py file

--- data.py
+++
@@ -1,5 +0,0 @@
-data = {
-    "p0_q1_0": "make sure to calculate cost based on weight AND price of each fruit.",
-    "p0_q2_0": "make sure to consider the total cost of the order for both apples AND oranges at each shop.",
-    "p0_q2_1": "make sure to consider the total cost of the order for both apples AND oranges at each shop."
-}
