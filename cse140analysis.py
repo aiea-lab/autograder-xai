6497ebc62b722bc69e85df31291be6bc61c94968
aoue <aajaouellette@gmail.com>
2024-04-08 17:06:27 -0700

    Prototype infrastructure for dynamic explanation

---
+++ cse140analysis.py
@@ -0,0 +1,6 @@
+class Analysis:
+    def __init__(self):
+        pass
+    def get_feedback(self, assignment, question):
+        feedback = f"dynamic feedback for {assignment} and {question}"
+        return feedback
--- explainer.py
+++ explainer.py
@@ -1,11 +1,20 @@
 import csv
 
+# by course
+# a single analyser can hold all the analysis functions for an course/PA?
+#   should instantiate a class by course, e.g. cse140_analyser()
+#   which will be stored in a separate file
+from autograderXAI import cse140analysis as analysisClass
+
 class Explainer:
     def __init__(self, course, assignment, question, csv_file):
+        # Linking question to spreadsheet answer and analysis
         self.course = course
         self.assignment = assignment
         self.question = question
         self.messages = []
+
+        # Spreadsheet (static explanations)
         self.data = {}
         with open(csv_file, newline='') as csvfile:
             reader = csv.reader(csvfile)
@@ -13,10 +22,20 @@ class Explainer:
                 key = ('_').join(row[0:3])
                 val = row[5]
                 self.data[key] = val
+        
+        # Analysis (dynamic explanations)
+        
+        self.analyser = analysisClass()
 
     # need to also implement the some_info input
     def get_feedback(self, default="TODO", issue_id=-1):
+        # Dynamic explanation
+        dynamic_exp = self.analyser.get_feedback(self.assignment, self.question)
+        return dynamic_exp
+
+        # Static explanation
         return default + "\n> " + self.get_explanation(issue_id)
+
     
     def get_explanation(self, issue_id):
         key = f"{self.assignment}_{self.question}_{issue_id}"
