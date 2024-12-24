f2b08bf341034ea87f1bb15819d56ff19ea1857d
BatuhanSA <basalihcs@gmail.com>
2024-12-23 22:39:25 -0800

    Added a test course for xia debugging

---
+++ test-exp/p1/grader.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+"""
+Grader for a sample assignment.
+
+We expect the student's code to provide the add() function,
+which should take two ints and return the sum of those two ints.
+
+This grader can be invoked using the autograder Python interface (e.g., `autograder.cli.grading.grade-dir`),
+or directly:
+```
+./grader.py -s my-submission-directory
+``
+"""
+
+import sys
+
+import autograder.assignment
+import autograder.cmd.gradeassignment
+import autograder.question
+
+from autograderxai import explainer
+# Create a class to represent the assignment.
+class Assignment1(autograder.assignment.Assignment):
+    def __init__(self, **kwargs):
+        super().__init__(questions = [
+            Add(10, "Question 1: Add"),
+        ], **kwargs)
+
+# Create a question for each thing we want to test.
+class Add(autograder.question.Question):
+    # Make a method to score the student's submission (provided in the |submission| argument).
+    def score_question(self, submission):
+        src='''def BFS(board: chess.Board, goal_position: chess.Square):
+        visited_nodes_in_order = []
+
+        # YOUR CODE HERE
+        seen_nodes = set()
+        seen_nodes.add(chess.A1)
+        queue = []
+        queue.append(chess.A1)
+
+        while queue:
+            curr_pos = queue.pop(0)
+            visited_nodes_in_order.append(curr_pos)
+            
+            board.set_piece_at(curr_pos, chess.Piece(chess.KNIGHT, chess.WHITE))
+            # print("*****")  
+            # print(board)
+            if goal_reached(board, goal_position):
+                break
+
+            legal_moves = [move.to_square for move in board.legal_moves]
+            
+            for move in legal_moves:
+                if move not in seen_nodes:
+                    seen_nodes.add(move)
+                    queue.append(move)
+
+            board.remove_piece_at(curr_pos)
+            # print(queue)
+        return visited_nodes_in_order
+        '''
+        exp = explainer.Analysis()
+        feedback = exp.get_feedback('bfs', src)
+        self.add_message(feedback)
+
+        # Setup test cases.
+        # [(a, b, case name), ...]
+        test_cases = [
+            (0, 0, 'zeros'),
+            (1, 1, 'basic'),
+            (-1, 1, 'negative'),
+        ]
+
+        # It can be useful to set the deduction automatically.
+        # Then if you add more test cases, you don't need to adjust point values.
+        deduction = -(max(1, self.max_points // len(test_cases)))
+
+        # Start everyone with full credit (you can also do the opposite)..
+        self.full_credit()
+
+        for (a, b, name) in test_cases:
+            # Reference the student's function using the __all__ lookup (it can also be referenced by package name).
+            actual = submission.__all__.add(a, b)
+            expected = a + b
+
+            if (actual != expected):
+                self.add_message("Add() did not return a correct result on test case '%s'." % (name), add_score = deduction)
+
+def main():
+    # The autograder library provides a default CLI for us.
+    return autograder.cmd.gradeassignment.main()
+
+if (__name__ == '__main__'):
+    sys.exit(main())
